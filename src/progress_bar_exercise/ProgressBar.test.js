294666951436227f3d13a04b713f86aedcbd564c
Matt Davis <matt@objectiveplay.com>
2022-01-01 19:12:05 -0700

    start and end request button test

--- src/progress_bar_exercise/ProgressBar.test.js
+++ src/progress_bar_exercise/ProgressBar.test.js
@@ -5,3 +5,19 @@ import { Solution } from './ProgressBarExercise';
 test("Progress bar solution renders", async () => {
     render(<Solution />);
 })
+
+test("Able to start and end requests", async () => {
+    render(<Solution />);
+    
+    expect(screen.getByTestId('request-button-start')).toBeEnabled();
+    expect(screen.getByTestId('request-button-end')).toBeDisabled();
+    fireEvent.click(screen.getByTestId('request-button-start'));
+
+    expect(screen.getByTestId('request-button-end')).toBeEnabled();
+    expect(screen.getByTestId('request-button-start')).toHaveTextContent('Loading...');
+    expect(screen.getByTestId('request-button-start')).toBeDisabled();
+
+    fireEvent.click(screen.getByTestId('request-button-end'));
+    expect(screen.getByTestId('request-button-end')).toBeDisabled();
+    expect(screen.getByTestId('request-button-start')).toHaveTextContent('Start Request');
+})
