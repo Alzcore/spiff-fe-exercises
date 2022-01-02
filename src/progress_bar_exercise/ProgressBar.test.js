d60c6d78924a3670929911e8ebe7bca5ccef41b2
Matt Davis <matt@objectiveplay.com>
2022-01-01 19:43:56 -0700

    Hang at 90% test 15 second >=

--- src/progress_bar_exercise/ProgressBar.test.js
+++ src/progress_bar_exercise/ProgressBar.test.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { render, screen, fireEvent} from '@testing-library/react'
 import { Solution } from './ProgressBarExercise';
 
+beforeEach(() => {  
+    jest.useFakeTimers();
+})
+
 test("Progress bar solution renders", async () => {
     render(<Solution />);
 })
@@ -20,4 +24,17 @@ test("Able to start and end requests", async () => {
     fireEvent.click(screen.getByTestId('request-button-end'));
     expect(screen.getByTestId('request-button-end')).toBeDisabled();
     expect(screen.getByTestId('request-button-start')).toHaveTextContent('Start Request');
+})
+
+test("Progress hangs at 90% at 15 seconds and longer", async () => {
+    render(<Solution />);
+    fireEvent.click(screen.getByTestId('request-button-start'));
+    
+    jest.advanceTimersByTime(15000);
+
+    expect(screen.getByTestId('progress-bar')).toHaveAttribute('value', '90');
+
+    jest.advanceTimersByTime(2000);
+
+    expect(screen.getByTestId('progress-bar')).toHaveAttribute('value', '90');
 })
