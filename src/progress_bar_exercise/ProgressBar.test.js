05009a7b815ddcf6708fd895adf3b60f4a78d472
Matt Davis <matt@objectiveplay.com>
2022-01-01 18:46:22 -0700

    Render test

---
+++ src/progress_bar_exercise/ProgressBar.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Solution } from './ProgressBarExercise';
+
+test("Progress bar solution renders", async () => {
+    render(<Solution />);
+})
