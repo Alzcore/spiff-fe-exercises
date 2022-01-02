3d34b5fdbff755a3508a78cf4304e878e0841277
Matt Davis <matt@objectiveplay.com>
2022-01-01 19:40:32 -0700

    fix imports on test

--- src/progress_bar_exercise/ProgressBar.test.js
+++ src/progress_bar_exercise/ProgressBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent} from '@testing-library/react'
 import { Solution } from './ProgressBarExercise';
 
 test("Progress bar solution renders", async () => {
